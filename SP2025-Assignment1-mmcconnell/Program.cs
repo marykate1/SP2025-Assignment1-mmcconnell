bc67e7a7b1dab27ef7f42ab9584753324f3923d6
Mary Kate <Mary Kate@WIN-HPCUSGS3BNC>
2025-01-26 21:06:43 -0800

    Add project files.

---
+++ SP2025-Assignment1-mmcconnell/Program.cs
@@ -0,0 +1,15 @@
+namespace SP2025_Assignment1_mmcconnell
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
